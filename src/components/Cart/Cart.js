bf9f1971fa558087ca627b50735e77ef3d56ca10
The-webtrue <developer.sojibdas@gmail.com>
2022-10-15 03:11:43 +0600

    added cart side

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+   
+    let total = 0;
+    let shipping = 0
+    for(const product of cart){
+        total += product.price;
+        shipping += product.shipping;
+    }
+    const tax = total * 0.1;
+
+    let grandTotal = total + shipping + tax;
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: ${cart.length}</p>
+            <p>Total Price: ${total} </p>
+            <p>Shipping Price: ${shipping} </p>
+            <p>Tax $ {tax.toFixed(2)} </p>
+            <h5>Grand Total: $ {grandTotal} </h5>
         </div>
     );
 };
